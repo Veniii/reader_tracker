ea0ea84e87b3a0b236340ac37dfc533e8fb0675e
Veni <9agata12@gmail.com>
2024-05-20 19:01:08 +0200

    scaffold with appbar and body

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('A.Reader')),
+      body: const Column(children: [Text("Hello")],),
+    );
   }
 }
