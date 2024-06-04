73a4a6b09e187aa6aa0efc672cadfb4f2971443e
Veni <9agata12@gmail.com>
2024-06-04 20:01:24 +0200

    added book image and titles to DetailsScreen

--- lib/screens/books_details.dart
+++ lib/screens/books_details.dart
@@ -15,10 +15,45 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
 
     final args = ModalRoute.of(context)!.settings.arguments as BookDetailsArguments;
     final Book book = args.itemBook;
+    final theme = Theme.of(context).textTheme;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(book.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            if (book.imageLinks.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Image.network(
+                  book.imageLinks['thumbnail'] ?? '',
+                  fit: BoxFit.cover
+                ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    book.title, 
+                    style: theme.headlineSmall,
+                  ),
+                  Text(
+                    book.authors.join(', '),
+                    style: theme.labelLarge,
+                  ),
+                  Text(
+                    'Published: ${book.publishedDate}',
+                    style: theme.bodySmall
+                  ),
+                  Text('Page Count: ${book.pageCount}', style: theme.bodySmall),
+                  Text('Language: ${book.language}', style: theme.bodySmall)
+                ],
+              ),
+            )
+          ],
+          ),
       ),
     );
   }
